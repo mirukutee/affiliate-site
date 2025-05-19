6afebbcfb383ac7f3a29acd8946661ad57259b16
mirukuteee <kawamurayuuji1998@gmail.com>
2025-05-19 20:25:23 +0900

    サイト更新

--- sidebar.js
+++ sidebar.js
@@ -1,6 +1,12 @@
 function toggleDropdown(id) {
-  document.querySelectorAll('.dropdown-content').forEach(el => {
-    if (el.id !== id) el.classList.remove('open');
+  const allDropdowns = document.querySelectorAll('.dropdown-content');
+
+  allDropdowns.forEach(drop => {
+    if (drop.id !== id) {
+      drop.classList.remove('open');
+    }
   });
-  document.getElementById(id).classList.toggle('open');
+
+  const current = document.getElementById(id);
+  current.classList.toggle('open');
 }

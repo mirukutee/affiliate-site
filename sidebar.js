e370b5ca3ca22be7c6d77e4488a291ab5366b667
mirukutee <kawamurayuuji1998@gmail.com>
2025-05-30 01:13:08 +0900

    サイト更新

--- sidebar.js
+++ sidebar.js
@@ -4,25 +4,29 @@ document.addEventListener("DOMContentLoaded", function () {
     .then(data => {
       document.getElementById('sidebar').innerHTML = data;
 
-      // ドロップダウンのイベントリスナーを設定
       const buttons = document.querySelectorAll(".dropdown-button");
 
       buttons.forEach(button => {
         button.addEventListener("click", function () {
           const content = this.nextElementSibling;
 
+          // 他を閉じる
+          document.querySelectorAll(".dropdown-content.open").forEach(c => {
+            if (c !== content) {
+              c.classList.remove("open");
+              c.style.maxHeight = null;
+              c.previousElementSibling.classList.remove("active"); // 対応するボタンも非アクティブ
+            }
+          });
+
           if (content.classList.contains("open")) {
             content.classList.remove("open");
             content.style.maxHeight = null;
+            this.classList.remove("active");
           } else {
-            // すでに開いているものを閉じる
-            document.querySelectorAll(".dropdown-content.open").forEach(c => {
-              c.classList.remove("open");
-              c.style.maxHeight = null;
-            });
-
             content.classList.add("open");
             content.style.maxHeight = content.scrollHeight + "px";
+            this.classList.add("active");
           }
         });
       });

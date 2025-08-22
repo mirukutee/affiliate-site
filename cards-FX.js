d3cebd3c6d8631ec96a9fdcc4b9f086c54815d17
mirukutee <kawamurayuuji1998@gmail.com>
2025-08-22 20:01:59 +0900

    繧ｵ繧､繝域峩譁ｰ

---
+++ cards-FX.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const target = document.getElementById("fx-list");
+
+  target.innerHTML = `
+  <div class="anime-item">
+   <section class="category-section" id="sec-fx">
+      <h2 class="linecenter">FX</h2>
+      <div class="my-card-grid">
+
+        <a href="https://mirukutee.github.io/affiliate-site/LION-FX.html" class="card">
+          <img src="Xcard/240_240.gif" alt="LION FXのバナー" loading="lazy" width="300" height="250">
+          <span>LION FX<br><small>スキャルピング対応</small></span>
+        </a>
+
+        <a href="https://mirukutee.github.io/affiliate-site/MUFGfx.html" class="card">
+          <img src="Xcard/300x250.jpg" alt="三菱UFJ eスマート証券FXのバナー" loading="lazy" width="300" height="250">
+          <span>三菱UFJ eスマート証券FX<small>デモ取引機能あり</small></span>
+        </a>
+
+        <a href="https://mirukutee.github.io/affiliate-site/fujimi-fx.html" class="card">
+          <img src="Xcard/fujitomi_system_trade_300x250.png" alt="シストレセレクト365のバナー" loading="lazy" width="300" height="250">
+          <span>シストレセレクト365<small>自動ツール系</small></span>
+        </a>
+
+        <a href="https://mirukutee.github.io/affiliate-site/himawari-fx-.html" class="card">
+          <img src="Xcard/imagesender.jfif" alt="ひまわり証券のバナー" loading="lazy" width="300" height="250">
+          <span>ひまわり証券<small>自動ツール系</small></span>
+        </a>
+
+        <a href="https://mirukutee.github.io/affiliate-site/matsui-FX.html" class="card">
+          <img src="Xcard/matsui-FX.png" alt="松井証券FXのバナー" loading="lazy" width="300" height="250">
+          <span>松井証券FX<small>自動ツール系</small></span>
+        </a>
+
+        <a href="https://mirukutee.github.io/affiliate-site/FINEST-FX.html" class="card">
+          <img src="Xcard/300x250 (1).jpg" alt="外為ファイネストのバナー" loading="lazy" width="300" height="250">
+          <span>外為ファイネスト<small>自動ツール系</small></span>
+        </a>
+
+        <a href="https://mirukutee.github.io/affiliate-site/saxo-fx.html" class="card">
+          <img src="Xcard/300x250_Affi-AT_FX-q3-swapv2_R.jpg" alt="サクソバンク証券FXのバナー" loading="lazy" width="300" height="250">
+          <span>サクソバンク証券FX<small>取引ツールが良質</small></span>
+        </a>
+
+      </div>
+    </section>
+  </div>
+  `;
+});
